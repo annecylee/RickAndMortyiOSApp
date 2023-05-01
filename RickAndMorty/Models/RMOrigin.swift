0f27fab6cf7ff611bc93faa733e5bbf05ce573b8
annecylee <annecylee@gmail.com>
2023-05-01 17:21:44 +0800

    Added models

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,8 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Tingwei Li on 5/1/23.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,8 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Tingwei Li on 5/1/23.
+//
+
+import Foundation
